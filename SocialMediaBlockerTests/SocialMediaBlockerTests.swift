a1164e0c7c6c49e504730a83423b1f89e743eed0
Charity Hipple <chipple3@gatech.edu>
2025-03-02 18:08:21 -0500

    Initial commit

---
+++ SocialMediaBlockerTests/SocialMediaBlockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaBlockerTests.swift
+//  SocialMediaBlockerTests
+//
+//  Created by Charity Hipple on 3/2/25.
+//
+
+import Testing
+@testable import SocialMediaBlocker
+
+struct SocialMediaBlockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
